772d3c40992d53210304fd9e32b203077e1c0d6a
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-03-23 07:06:24 +0530

    React HW-01

---
+++ src/Components/HW1.jsx
@@ -0,0 +1,16 @@
+
+function HW1() {
+  return (
+    <div>
+      <h1>Topics covered</h1>
+      <h3><a href="#">Getting started with the web</a></h3>
+      <p>provides a pratical introduction to web development for complete beginners.</p>
+      <h3><a href='#'>HTML - Structuring the web</a></h3>
+      <p>HTML is use for structuring the Website. </p>
+      <h3><a href="#">CSS - Styling the Web</a></h3>
+      <p>Css is use to styling the website</p>
+    </div>
+  );
+}
+
+export default HW1;
